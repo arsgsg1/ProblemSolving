4a2733e0ee9914a80cdf5c438f9576cb1e82de33
jhyun <jhyun@comtrue.com>
2020-02-27 19:57:25 +0900

    2229 조짜기

---
+++ 2229/2229.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#define MAX_LEN 10001
+int arr[MAX_LEN];
+int d[MAX_LEN];
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+		int Max = -1, Min = MAX_LEN;
+		for (int j = i; j > 0; j--) {
+			Max = max(Max, arr[j]);
+			Min = min(Min, arr[j]);
+			d[i] = max(d[i], Max - Min + d[j - 1]);
+		}
+	}
+	printf("%d\n", d[n]);
+	return 0;
+}
