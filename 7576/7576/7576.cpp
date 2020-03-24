a87f14a1e0da496ced3b8bbe48aac94cc1c2b36c
jhyun <arsgsg1@gmail.com>
2020-03-24 18:33:41 +0900

    [DBG] 7576 메모리초과

--- 7576/7576/7576.cpp
+++ 7576/7576/7576.cpp
@@ -1,11 +1,23 @@
-﻿#include <iostream>
-#include <queue>
-#include <algorithm>
+﻿#include <iostream> 
+#include <queue> 
+#include <algorithm> 
 using namespace std;
-#define MAX_LEN 1000
 int dir_x[] = { -1, 0, 1, 0 };
 int dir_y[] = { 0, 1, 0, -1 };
-int Solution(queue<pair<pair<int, int>, int>>& q, int field[][MAX_LEN], int width, int height)
+
+bool IsImpossible(vector<vector<int>>& field, int width, int height)
+{
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if (field[i][j] == 0) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+int Solution(queue<pair<pair<int, int>, int>>& q, vector<vector<int>>& field, int width, int height)
 {
 	pair<int, int> p;
 	int cur_x, cur_y, cnt = 0, day = 0;
@@ -15,35 +27,43 @@ int Solution(queue<pair<pair<int, int>, int>>& q, int field[][MAX_LEN], int widt
 		cur_x = q.front().first.first;
 		cur_y = q.front().first.second;
 		day = q.front().second;
+		q.pop();
 		if (field[cur_x][cur_y] == 0)
 			field[cur_x][cur_y] = day;
 		for (int i = 0; i < 4; i++) {
 			next_x = cur_x + dir_x[i];
 			next_y = cur_y + dir_y[i];
-			if (next_x < width && next_y < height) {
+			if (next_x < height && next_y < width && next_x >= 0 && next_y >= 0) {
 				if (field[next_x][next_y] == 0) {
 					q.push(make_pair(make_pair(next_x, next_y), day + 1));
 				}
 			}
 		}
 	}
-	return day;
+	if (IsImpossible(field, width, height))
+		return -1;
+	else
+		return day;
 }
 
 int main()
 {
-	int field[MAX_LEN][MAX_LEN];
-	int width, height;
+	vector<vector<int>> field;
+	vector<int> s;
+	int width, height, temp;
 	queue<pair<pair<int, int>, int>> q;
 	cin >> width >> height;
 	for (int i = 0; i < height; i++) {
-		for (int j = 0; i < width; j++) {
-			cin >> field[i][j];
-			if (field[i][j] == 1) {
+		for (int j = 0; j < width; j++) {
+			cin >> temp;
+			s.push_back(temp);
+			if (temp == 1) {
 				q.push(make_pair(make_pair(i, j), 0));
 			}
 		}
+		field.push_back(s);
+		s.clear();
 	}
-	Solution(q, field, width, height);
+	printf("%d\n", Solution(q, field, width, height));
 	return 0;
 }
