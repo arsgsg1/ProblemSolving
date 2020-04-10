9b51380555f4e66e3ed07d02fec14f51f684351a
jhyun <arsgsg1@gmail.com>
2020-04-10 13:42:01 +0900

    [CLR] 1726 로봇
    코딩 실수 줄이기

--- 1726/1726/1726.cpp
+++ 1726/1726/1726.cpp
@@ -1,102 +1,79 @@
 ﻿#include <iostream>
-#include <algorithm>
 #include <queue>
 using namespace std;
+int dirRow[] = { 0, 1, -1, 0, 0 };
+int dirCol[] = { 0, 0, 0, 1, -1 };
+int bfs[5][102][102] = { {false, false, false} };
+int field[102][102];
+int row, col;
 typedef struct {
 	int row;
 	int col;
 	int dir;
-}DIR;
-#define EAST 1
-#define WEST 2
-#define SOUTH 3
-#define NORTH 4
-int dir[] = { EAST, WEST, SOUTH, NORTH };
-int dx[] = { 0, 1, -1, 0, 0 };
-int dy[] = { 0, 0, 0, 1, -1 };	//동서남북
-int field[100][100] = { {0, 0}, };
+}_TERM;
 
-void Print(int width, int height)
+int Solution(queue<_TERM>& q, const _TERM& goal)
 {
-	printf("===========\n");
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
-			printf("%d ", field[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-int Solution(queue<DIR>& q, int width, int height, DIR goal)
-{
-	DIR cur, next;
+	_TERM term, next;
 	while (!q.empty())
 	{
-		cur = q.front();
+		term = q.front();
 		q.pop();
-		if (cur.row == goal.row && cur.col == goal.col) {
-			//좌표에 도달했을 때
-			//방향 비교
-			Print(width, height);
-			if (cur.dir == goal.dir) {
-			}
-			else if ((cur.dir == 1 && goal.dir == 2) || (cur.dir == 2 && goal.dir == 1) ||
-				(cur.dir == 3 && goal.dir == 4) || (cur.dir == 4 && goal.dir == 3)) {
-				field[cur.row][cur.col] += 2;
-			}
-			else {
-				field[cur.row][cur.col] += 1;
-			}
-			Print(width, height);
-			break;
+		if (goal.row == term.row && goal.col == term.col && goal.dir == term.dir) {
+			return bfs[goal.dir][goal.row][goal.col] - 1;
 		}
-		
-		for (int i = 0; i < 4; i++) {
-			for (int j = 1; j <= 3; j++) {
-				next.col = cur.col + dx[dir[i]] * j;
-				next.row = cur.row + dy[dir[i]] * j;
-				next.dir = dir[i];
-				if (next.row >= width || next.col >= height || next.col < 0 || next.row < 0)
-					break;
-
-				if (field[next.row][next.col] == 0) {
-					//방향에 따른 명령횟수 증가 및 방문처리
-					if (next.dir == cur.dir) {
-						field[next.row][next.col] += field[cur.row][cur.col] + 1;
-					}
-					else {
-						field[next.row][next.col] += field[cur.row][cur.col] + 2;
-					}
-					q.push(next);
+		//Go
+		for (int i = 1; i <= 3; i++) {
+			next.row = term.row + dirCol[term.dir] * i;
+			next.col = term.col + dirRow[term.dir] * i;
+			next.dir = term.dir;
+			if (next.row < 1 || next.col < 1 ||
+				next.row > row || next.col > col ||
+				field[next.row][next.col] != 0) {
+				//범위를 벗어나거나 벽을 통과할순 없으므로 중단
+				break;
+			}
+			if (bfs[next.dir][next.row][next.col] == 0) {
+				q.push(next);
+				bfs[next.dir][next.row][next.col] = bfs[term.dir][term.row][term.col] + 1;
+			}
+		}
+		//Turn Direction
+		for (int i = 1; i <= 4; i++) {
+			if (bfs[i][term.row][term.col] == 0) {
+				next.row = term.row;
+				next.col = term.col;
+				next.dir = i;
+				q.push(next);
+				//방향에 따른 명령 횟수 증가
+				if ((term.dir == 1 && i == 2) || (term.dir == 3 && i == 4)) {
+					//반대방향
+					bfs[next.dir][next.row][next.col] = bfs[term.dir][term.row][term.col] + 2;
 				}
 				else {
-					continue;
+					//수직방향
+					bfs[next.dir][next.row][next.col] = bfs[term.dir][term.row][term.col] + 1;
 				}
 			}
 		}
 	}
-	return field[goal.row][goal.col] - 1;
+	return 0;
 }
 
 int main()
 {
-	queue<DIR> q;
-	DIR start, goal;
-	int width, height;
-	cin >> height >> width;
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
+	_TERM term;
+	queue<_TERM> q;
+	cin >> row >> col;
+	for (int i = 1; i <= row; i++) {
+		for (int j = 1; j <= col; j++) {
 			cin >> field[i][j];
 		}
 	}
-	cin >> start.row >> start.col >> start.dir;
-	cin >> goal.row >> goal.col >> goal.dir;
-	start.row--;
-	start.col--;
-	goal.row--;
-	goal.col--;
-	field[start.row][start.col] = 1;
-	q.push(start);
-	printf("%d", Solution(q, width, height, goal));
+	cin >> term.row >> term.col >> term.dir;
+	bfs[term.dir][term.row][term.col] = 1;
+	q.push(term);
+	cin >> term.row >> term.col >> term.dir;
+	printf("%d\n", Solution(q, term));
 	return 0;
 }
