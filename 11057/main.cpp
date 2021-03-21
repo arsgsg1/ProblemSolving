28019dffa600eef66c3a59b50f0ccaaeb6c5c108
arsgsg1 <arsgsg1@gmail.com>
2021-03-21 13:43:54 +0900

    [UPD] 11057 오르막 수

--- 11057/main.cpp
+++ 11057/main.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
-
 using namespace std;
-
-int main() {
-	int DP[1001][10] = {};
-	int ans, N;
-	ans = 0;
-
-	scanf("%d", &N);
-
-	for (int i = 0; i < 10; i++) {
-		DP[1][i] = 1;
-	}
-
+#define CONST 10007
+int N;
+int dp[1001][1001];
+/*
+dp[N][M] = 길이 M이고 마지막 수가 N인 오르막 수의 개수
+dp[N][M] = dp[N - i][M - 1]
+*/
+int main()
+{
+	cin >> N;
+	for (int i = 0; i < 10; i++)
+		dp[i][1] = 1;
 	for (int i = 2; i <= N; i++) {
-		for (int j = 0; j < 10; j++) {
-			for (int k = j; k < 10; k++) {
-				DP[i][j] = (DP[i][j] + DP[i - 1][k]) % 10007;
+		for (int k = 9; k >= 0; k--) {
+			for (int j = 0; j <= k; j++) {
+				dp[k][i] += dp[j][i - 1] % CONST;
 			}
 		}
 	}
-
-	for (int i = 0; i < 10; i++) {
-		ans = (ans + DP[N][i]) % 10007;
-	}
-
-	printf("%d\n", ans);
-
+	int answer = 0;
+	for (int i = 0; i <= 9; i++)
+		answer += dp[i][N] % CONST;
+	printf("%d\n", answer % CONST);
 	return 0;
 }
