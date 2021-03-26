b5542c47eb3604db8092e7b3cea6814039743749
arsgsg1 <arsgsg1@gmail.com>
2021-03-26 12:11:10 +0900

    [UPD] 1005 주석추가

--- 1005/main.cpp
+++ 1005/main.cpp
@@ -1,51 +1,53 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
 #include <string.h>
+#include <algorithm>
 using namespace std;
-const int MAX_SIZE = 10001;
+#define MAX_SIZE 10001
 int N, K;
-vector<int> graph[MAX_SIZE + 1];
-int dur[MAX_SIZE + 1];
-int inDegree[MAX_SIZE + 1];
-long long result[MAX_SIZE + 1];
+int dur[MAX_SIZE];
+int inDegree[MAX_SIZE];
+int result[MAX_SIZE];
+vector<int> graph[MAX_SIZE];
 void topology_sort()
 {
 	queue<int> q;
-	for (int i = 1; i <= N; i++) {
+	for (int i = 1; i <= N; i++)
+		//in-count가 0인 노드들을 큐에 넣는다.
 		if (inDegree[i] == 0)
 			q.push(i);
-	}
 	while (!q.empty()) {
 		int cur = q.front();
 		q.pop();
 		for (int i = 0; i < graph[cur].size(); i++) {
 			int next = graph[cur][i];
+			//현재까지 찾은 최댓값보다 지금 값이 더 크다면 갱신
 			result[next] = max(result[next], result[cur] + dur[cur]);
-			if (--inDegree[next] == 0)
+			//다시 in-count가 0인 노드들을 큐에 넣음
+			if (--inDegree[next] == 0) {
 				q.push(next);
+			}
 		}
 	}
 }
 int main()
 {
-	int tc;
-	int s, e, goal;
+	int tc, s, e, goal;
 	cin >> tc;
-	while (tc--) {
+	for (int t = 0; t < tc; t++) {
 		cin >> N >> K;
-		memset(inDegree, 0, sizeof(inDegree));
-		memset(result, 0, sizeof(result));
 		memset(dur, 0, sizeof(dur));
-		for (int i = 1; i <= N; i++)
-			graph[i].clear();
-		for (int i = 1; i <= N; i++)
+		memset(result, 0, sizeof(result));
+		memset(inDegree, 0, sizeof(inDegree));
+		for (int i = 1; i <= N; i++) {
 			cin >> dur[i];
+			graph[i].clear();
+		}
 		for (int i = 1; i <= K; i++) {
 			cin >> s >> e;
-			inDegree[e]++;
 			graph[s].push_back(e);
+			inDegree[e]++;
 		}
 		cin >> goal;
 		topology_sort();
