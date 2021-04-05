8e1fab8ded0d4fcf179d270dddb01b5f63a67528
arsgsg1 <arsgsg1@gmail.com>
2021-04-05 11:36:50 +0900

    [UPD] 14501 퇴사

--- 14501/14501/14501.cpp
+++ 14501/14501/14501.cpp
@@ -1,31 +1,21 @@
 ﻿#include <iostream>
 #include <algorithm>
-#define MAX_LEN 1001
 using namespace std;
-
-int Solution(int n, int dp[], int ta[], int pa[])
-{
-	int Max = 0;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 0; j <= i; j++) {
-			if (j + ta[j] <= n) {
-				dp[j + ta[j]] = max(dp[j] + pa[j], dp[j + ta[j]]);
-			}
-			dp[j + 1] = max(dp[j], dp[j + 1]);
-		}
-	}
-	printf("%d", *max_element(dp, dp + n + 1));
-	return 0;
-}
+int N;
+int ti[17];
+int pi[17];
+int dp[2000];
 int main()
 {
-	int ta[MAX_LEN] = { 0, }, pa[MAX_LEN] = { 0, }, dp[MAX_LEN] = { 0, };
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> ta[i];
-		cin >> pa[i];
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> ti[i] >> pi[i];
+	for (int i = 0; i <= N; i++) {
+		int next = i + ti[i];
+		dp[next] = max(dp[next], dp[i] + pi[i]);
+		dp[i + 1] = max(dp[i], dp[i + 1]);
 	}
-	Solution(n, dp, ta, pa);
+	int answer = *max_element(dp, dp + N + 1);
+	printf("%d\n", answer);
 	return 0;
 }
