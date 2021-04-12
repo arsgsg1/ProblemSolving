21ced6075dc3fae9a2791258c56a76c6483a4650
arsgsg1 <arsgsg1@gmail.com>
2021-04-12 13:37:46 +0900

    [UPD] 14888 연산자 끼워넣기

--- 14888/14888/main.cpp
+++ 14888/14888/main.cpp
@@ -1,39 +1,51 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
-const int MAX = 1000000000 + 1;
-int N;
-int number[12], Operator[4];
-int maxResult = -MAX, minResult = MAX;
-void DFS(int plus, int minus, int multiply, int divide, int cnt, int sum)
+int N, MinResult = 1000000002;
+int MaxResult = -1000000002;
+int arr[11];
+vector<int> oper(4, 0);
+void dfs(int op_idx, int num_idx, int sum)
 {
-	//연산자를 모두 사용하였을 경우
-	if (cnt == N)
-	{
-		maxResult = max(maxResult, sum);
-		minResult = min(minResult, sum);
+	if (num_idx == N - 1) {
+		MaxResult = max(sum, MaxResult);
+		MinResult = min(sum, MinResult);
+		return;
 	}
-	//모든 경우를 고려
-	if (plus > 0)
-		DFS(plus - 1, minus, multiply, divide, cnt + 1, sum + number[cnt]);
-	if (minus > 0)
-		DFS(plus, minus - 1, multiply, divide, cnt + 1, sum - number[cnt]);
-	if (multiply > 0)
-		DFS(plus, minus, multiply - 1, divide, cnt + 1, sum * number[cnt]);
-	if (divide > 0)
-		DFS(plus, minus, multiply, divide - 1, cnt + 1, sum / number[cnt]);
+	for (int i = 0; i < 4; i++) {
+		if (oper[i] <= 0)
+			continue;
+		if (i == 0) {
+			oper[i]--;
+			dfs(i, num_idx + 1, sum + arr[num_idx + 1]);
+			oper[i]++;
+		}
+		else if (i == 1) {
+			oper[i]--;
+			dfs(i, num_idx + 1, sum - arr[num_idx + 1]);
+			oper[i]++;
+		}
+		else if (i == 2) {
+			oper[i]--;
+			dfs(i, num_idx + 1, sum * arr[num_idx + 1]);
+			oper[i]++;
+		}
+		else if (i == 3) {
+			oper[i]--;
+			dfs(i, num_idx + 1, sum / arr[num_idx + 1]);
+			oper[i]++;
+		}
+	}
 }
-int main(void)
+int main()
 {
 	cin >> N;
 	for (int i = 0; i < N; i++)
-		cin >> number[i];
+		cin >> arr[i];
 	for (int i = 0; i < 4; i++)
-		cin >> Operator[i];
-
-	DFS(Operator[0], Operator[1], Operator[2], Operator[3], 1, number[0]);
-	cout << maxResult << endl;
-	cout << minResult << endl;
+		cin >> oper[i];
+	dfs(0, 0, arr[0]);
+	printf("%d\n%d\n", MaxResult, MinResult);
 	return 0;
-
 }
