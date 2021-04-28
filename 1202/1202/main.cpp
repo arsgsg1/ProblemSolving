2c94313bff2fe46cdb056849db2d0c86e2c991cf
arsgsg1 <arsgsg1@gmail.com>
2021-04-28 23:52:38 +0900

    [UPD] 1202 보석도둑

--- 1202/1202/main.cpp
+++ 1202/1202/main.cpp
@@ -4,36 +4,32 @@
 using namespace std;
 #define MAX_SIZE 300000
 pair<int, int> jew[MAX_SIZE];
-int bp[MAX_SIZE] = { 0, };
+int bp[MAX_SIZE];
 int N, K;
 int main()
 {
 	cin >> N >> K;
+	int m, v, c;
+	long long result = 0;
+	priority_queue<int> pq;
+	int idx = 0;
 	for (int i = 0; i < N; i++) {
 		cin >> jew[i].first >> jew[i].second;
 	}
 	for (int i = 0; i < K; i++) {
 		cin >> bp[i];
 	}
-	
-	//무게 제한이 낮은 가방이 가장 비싼 보석을 넣도록 정렬
 	sort(jew, jew + N);
 	sort(bp, bp + K);
 
-	long long result = 0;
-	int idx = 0;
-	priority_queue<int> pq;
-
 	for (int i = 0; i < K; i++) {
-		//
 		while (idx < N && jew[idx].first <= bp[i])
 			pq.push(jew[idx++].second);
-
-		if (!pq.empty()) {
+		if (pq.size()) {
 			result += pq.top();
 			pq.pop();
 		}
 	}
-	cout << result;
+	cout << result << endl;
 	return 0;
 }
