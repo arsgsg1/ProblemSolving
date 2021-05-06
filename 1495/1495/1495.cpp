54b10507302afb004b7edb34f857de69f41c7e89
arsgsg1 <arsgsg1@gmail.com>
2021-05-06 22:10:33 +0900

    [UPD] 1495 기타리스트

--- 1495/1495/1495.cpp
+++ 1495/1495/1495.cpp
@@ -1,28 +1,28 @@
 ﻿#include <iostream>
 #include <algorithm>
 using namespace std;
+#define MAX_SIZE 1001
 int N, S, M;
 int arr[101];
-int dp[101][1001];
+int dp[101][MAX_SIZE];
 int main()
 {
 	cin >> N >> S >> M;
-	dp[0][S] = 1;
-	dp[0][S] = 1;
 	for (int i = 1; i <= N; i++)
 		cin >> arr[i];
+	dp[0][S] = 1;
 	for (int i = 1; i <= N; i++) {
 		for (int j = 0; j <= M; j++) {
-			if (!dp[i - 1][j])
+			if (dp[i - 1][j] == 0)
 				continue;
 			if (j - arr[i] >= 0)
-				dp[i][j - arr[i]] = true;
+				dp[i][j - arr[i]] = 1;
 			if (j + arr[i] <= M)
-				dp[i][j + arr[i]] = true;
+				dp[i][j + arr[i]] = 1;
 		}
 	}
 	for (int i = M; i >= 0; i--) {
-		if (dp[N][i]) {
+		if (dp[N][i] == 1) {
 			printf("%d\n", i);
 			return 0;
 		}
