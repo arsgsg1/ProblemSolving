cb2a3f3320c0259981f894c3fe207c8a546e159f
arsgsg1 <arsgsg1@gmail.com>
2021-05-09 16:42:56 +0900

    [CLR] 1208 부분수열의 합2

---
+++ 1208/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+vector<int> v;
+int n, s, half;
+long long ans;
+map<int, int> mp;
+
+void dfsLeft(int idx, int sum) {
+	if (idx == half) {
+		mp[sum]++; return;
+	}
+	dfsLeft(idx + 1, sum);
+	dfsLeft(idx + 1, sum + v[idx]);
+}
+
+void dfsRight(int idx, int sum) {
+	if (idx == n) {
+		ans += mp[s - sum]; return;
+	}
+	dfsRight(idx + 1, sum);
+	dfsRight(idx + 1, sum + v[idx]);
+}
+
+int main() {
+	cin >> n >> s;
+	half = n / 2; v.resize(n);
+	for (int i = 0; i<n; i++) cin >> v[i];
+	dfsLeft(0, 0);
+	dfsRight(half, 0);
+	if (s == 0) ans--;
+	cout << ans;
+}
