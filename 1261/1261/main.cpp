12b069cfa03ae270bba8f123bfe7b98eeff2836b
arsgsg1 <arsgsg1@gmail.com>
2021-06-23 14:17:05 +0900

    [UPD] 1261 알고스팟

--- 1261/1261/main.cpp
+++ 1261/1261/main.cpp
@@ -1,19 +1,21 @@
+/*
+벽을 최소로 부수는 것 = 빈 칸을 우선하여 탐색
+벽과 빈 칸 중에 우선하여 탐색하는 방법 => 자료구조 우선순위 큐 혹은 덱으로 구현 가능
+*/
 #include <iostream>
-#include <string>
 #include <deque>
+#include <string>
 using namespace std;
-#define MAX_SIZE 100
-string arr[MAX_SIZE];
-int MAX_COL, MAX_ROW;
-int crash[MAX_SIZE][MAX_SIZE];
-bool visit[MAX_SIZE][MAX_SIZE];
+int Width, Height;
+int field[101][101];
+int dist[101][101];
 int dirRow[] = { 1, 0, -1, 0 };
 int dirCol[] = { 0, 1, 0, -1 };
-deque<pair<int, int>> dq;
 void bfs(int row, int col)
 {
+	deque<pair<int, int>> dq;
+	dist[row][col] = 1;
 	dq.push_back({ row, col });
-	visit[row][col] = true;
 	int r, c, nr, nc;
 	while (!dq.empty()) {
 		r = dq.front().first;
@@ -22,27 +24,35 @@ void bfs(int row, int col)
 		for (int i = 0; i < 4; i++) {
 			nr = r + dirRow[i];
 			nc = c + dirCol[i];
-			if (nr < 0 || nc < 0 || nr >= MAX_ROW || nc >= MAX_COL)
+			if (nr <= 0 || nc <= 0 || nr > Height || nc > Width)
 				continue;
-			if (arr[nr][nc] == '0' && visit[nr][nc] == false) {
-				dq.push_front({ nr, nc });
-				visit[nr][nc] = true;
-				crash[nr][nc] = crash[r][c];
-			}
-			else if (arr[nr][nc] == '1' && visit[nr][nc] == false) {
-				dq.push_back({ nr, nc });
-				visit[nr][nc] = true;
-				crash[nr][nc] = crash[r][c] + 1;
+			if (dist[nr][nc] == 0) {
+				if (field[nr][nc] == 0) {
+					dist[nr][nc] = dist[r][c];
+					dq.push_front({ nr, nc });
+				}
+				else {
+					dist[nr][nc] = dist[r][c] + 1;
+					dq.push_back({ nr, nc });
+				}
 			}
 		}
 	}
 }
 int main()
 {
-	cin >> MAX_COL >> MAX_ROW;
-	for (int i = 0; i < MAX_ROW; i++)
-		cin >> arr[i];
-	bfs(0, 0);
-	printf("%d\n", crash[MAX_ROW - 1][MAX_COL - 1]);
+	cin >> Width >> Height;
+	string buf;
+	for (int i = 1; i <= Height; i++) {
+		cin >> buf;
+		for (int j = 0; j < buf.length(); j++) {
+			if (buf[j] == '1')
+				field[i][j + 1] = 1;
+			else
+				field[i][j + 1] = 0;
+		}
+	}
+	bfs(1, 1);
+	printf("%d\n", dist[Height][Width] - 1);
 	return 0;
 }
