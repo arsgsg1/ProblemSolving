9471803f2d73502bbd35bacf7240e84ed85319aa
arsgsg1 <arsgsg1@gmail.com>
2021-06-23 13:45:53 +0900

    [UPD] 2206 벽 부수고 이동하기

--- 2206/2206/main.cpp
+++ 2206/2206/main.cpp
@@ -1,74 +1,63 @@
 #include <iostream>
-#include <string>
 #include <queue>
+#include <string>
+#include <algorithm>
 using namespace std;
-bool bfs[2][1002][1002] = { {false, false, false} };
-int field[1002][1002] = { {0,0} };
-int n, m;
-int dirRow[] = { 1, -1, 0, 0 };
-int dirCol[] = { 0, 0, 1, -1 };
-typedef struct {
-	int block;
-	int row;
-	int col;
-	int cnt;
-}VERTEX;
-int Solution(queue<VERTEX>& q)
+#define INF 987654321
+int N, M;
+int field[1001][1001];
+int dist[1001][1001][2];
+int dirRow[] = { 1, 0, -1, 0 };
+int dirCol[] = { 0, 1, 0, -1 };
+void bfs(int row, int col)
 {
-	VERTEX v, next;
-	while (!q.empty())
-	{
-		v = q.front();
+	int r, c, nr, nc, b = 0;
+	queue<pair<int, pair<int, int>>> q;
+	dist[row][col][0] = 1;
+	dist[row][col][1] = 1;
+	q.push({ b, {row, col}});
+	while (!q.empty()) {
+		r = q.front().second.first;
+		c = q.front().second.second;
+		b = q.front().first;
 		q.pop();
-		if (n == v.row && m == v.col) {
-			return v.cnt;
-		}
 		for (int i = 0; i < 4; i++) {
-			next.row = v.row + dirRow[i];
-			next.col = v.col + dirCol[i];
-			//범위 초과
-			if (next.row < 1 || next.col < 1 ||
-				next.row > n || next.col > m) {
+			nr = r + dirRow[i];
+			nc = c + dirCol[i];
+			if (nr <= 0 || nc <= 0 || nr > N || nc > M)
 				continue;
+			//벽이 아니고, 방문하지 않은 칸이라면
+			if (field[nr][nc] == 0 && dist[nr][nc][b] == 0) {
+				dist[nr][nc][b] = dist[r][c][b] + 1;
+				q.push({ b, {nr, nc} });
 			}
-			//벽을 부순 상태거나 벽을 부수지 않은 상태에서 방문하지 않은 항이라면
-			if (field[next.row][next.col] == 0 &&
-				bfs[v.block][next.row][next.col] == false) {
-				next.block = v.block;
-				next.cnt = v.cnt + 1;
-				q.push(next);
-				bfs[next.block][next.row][next.col] = true;
-			}
-			//벽을 만났고 벽을 부수지 않은 상태라면
-			else if (field[next.row][next.col] == 1 && v.block == 0) {
-				next.block = 1;
-				next.cnt = v.cnt + 1;
-				q.push(next);
-				bfs[next.block][next.row][next.col] = true;
+			//벽을 부술 수 있고, 벽이며, 방문하지 않았다면
+			else if (b == 0 && field[nr][nc] == 1 && dist[nr][nc][1] == 0) {
+				dist[nr][nc][1] = dist[r][c][0] + 1;
+				q.push({ 1, {nr, nc} });
 			}
 		}
 	}
-	return -1;
 }
-
 int main()
 {
-	queue<VERTEX> q;
-	cin >> n >> m;
-	string str;
-	for (int i = 1; i <= n; i++) {
-		cin >> str;
-		for (int j = 0; j < str.length(); j++) {
-			field[i][j + 1] = str[j] - '0';
+	cin >> N >> M;
+	string buf;
+	for (int i = 0; i < N; i++) {
+		cin >> buf;
+		for (int j = 0; j < buf.length(); j++) {
+			if (buf[j] == '1')
+				field[i + 1][j + 1] = 1;
+			else
+				field[i + 1][j + 1] = 0;
 		}
 	}
-	VERTEX v;
-	v.row = 1;
-	v.col = 1;
-	v.cnt = 1;
-	v.block = 0;
-	bfs[v.block][v.row][v.col] = true;
-	q.push(v);
-	printf("%d\n", Solution(q));
+	bfs(1, 1);
+	dist[N][M][0] = dist[N][M][0] == 0 ? INF : dist[N][M][0];
+	dist[N][M][1] = dist[N][M][1] == 0 ? INF : dist[N][M][1];
+	if (dist[N][M][0] == INF && dist[N][M][1] == INF)
+		printf("%d\n", -1);
+	else
+		printf("%d\n", min(dist[N][M][0], dist[N][M][1]));
 	return 0;
 }
