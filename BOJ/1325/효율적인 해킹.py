719b2c75907c127ebf23cc163976a4cd20cabf8f
arsgsg1 <arsgsg1@gmail.com>
2022-03-07 19:31:48 +0900

    효율적인 해킹

--- BOJ/1325/효율적인 해킹.py
+++ BOJ/1325/효율적인 해킹.py
@@ -1,15 +1,19 @@
 import sys
 from collections import defaultdict, deque
 ans_dict = defaultdict(list)
-input = sys.stdin.readline
-N, M = map(int, input().split())
 graph = [[] for _ in range(N + 1)]
+input = sys.stdin.readline
+#define end
+N, M = map(int, input().split())
+for _ in range(M):
+    e, s = map(int, input().split())
+    graph[s].append(e)
+#input end
 def bfs(start: int) -> int: #노드 개수를 반환
     global N
     cnt = 0
     visit = [False for _ in range(N + 1)]
-    q = deque([])
-    q.append(start)
+    q = deque([start])
     visit[start] = True
     while q:
         cur = q.popleft()
@@ -19,12 +23,9 @@ def bfs(start: int) -> int: #노드 개수를 반환
                 visit[nxt] = True
                 q.append(nxt)
     return cnt
-
-for _ in range(M):
-    e, s = map(int, input().split())
-    graph[s].append(e)
-
+#function end
 for i in range(1, N + 1):
     ans_dict[bfs(i)].append(i)
 l = sorted(ans_dict.keys(), reverse=True)
 print(*ans_dict[l[0]])
+#main end
